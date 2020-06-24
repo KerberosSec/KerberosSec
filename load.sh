bb7648d139123f2b517a606b84c140cba4106945
Diego333-ms <66188985+Diego333-ms@users.noreply.github.com>
2020-06-24 16:00:47 -0300

    Add files via upload

---
+++ load.sh
@@ -0,0 +1,140 @@
+#!/bin/bash
+
+#Barra de Carregamento
+
+load() {
+
+printf "\n\033[01;33m Carregando Kerberos...\n"
+sleep 2
+
+clear
+
+printf "\n\033[01;33m Carregando Kerberos...\n"
+printf "[..........................................] 0*\n"
+
+sleep 0.3
+clear
+
+printf "\n\033[01;33m Carregando Kerberos...\n"
+printf "[#.........................................] 1*\n"
+
+sleep 0.3
+clear
+
+printf "\n\033[01;33m Carregando Kerberos...\n"
+printf "[##.......................................] 2*\n"
+
+sleep 0.3
+clear
+
+printf "\n\033[01;33m Carregando Kerberos...\n"
+printf "[#########................................] 10*\n"
+
+sleep 0.3
+clear
+
+printf "\n\033[01;33m Carregando Kerberos...\n"
+printf "[###########..............................] 15*\n"
+
+sleep 0.3
+clear
+
+printf "\n\033[01;33m Carregando Kerberos...\n"
+printf "[############.............................] 16*\n"
+
+sleep 0.3
+clear
+
+printf "\n\033[01;33m Carregando Kerberos...\n"
+printf "[##############...........................] 18*\n"
+
+sleep 0.3
+clear
+
+printf "\n\033[01;33m Carregando Kerberos...\n"
+printf "[###############..........................] 20*\n"
+
+sleep 0.3
+clear
+
+printf "\n\033[01;33m Carregando Kerberos...\n"
+printf "[#################........................] 25*\n"
+
+sleep 0.3
+clear
+
+printf "\n\033[01;33m Carregando Kerberos...\n"
+printf "[##################.......................] 27*\n"
+
+sleep 0.3
+clear
+
+printf "\n\033[01;33m Carregando Kerberos...\n"
+printf "[#####################....................] 30*\n"
+
+sleep 0.3
+clear
+
+printf "\n\033[01;33m Carregando Kerberos...\n"
+printf "[######################...................] 35*\n"
+
+sleep 0.3
+clear
+
+printf "\n\033[01;33m Carregando Kerberos...\n"
+printf "[###########################..............] 54*\n"
+
+sleep 0.3
+clear
+
+printf "\n\033[01;33m Carregando Kerberos...\n"
+printf "[############################.............] 55*\n"
+
+sleep 0.3
+clear
+
+printf "\n\033[01;33m Carregando Kerberos...\n"
+printf "[##############################...........] 70*\n"
+
+sleep 0.3
+clear
+
+printf "\n\033[01;33m Carregando Kerberos...\n"
+printf "[################################.........] 75*\n"
+
+sleep 0.3
+clear
+
+printf "\n\033[01;33m Carregando Kerberos...\n"
+printf "[#################################........] 90*\n"
+
+sleep 0.3
+clear
+
+printf "\n\033[01;33m Carregando Kerberos...\n"
+printf "[####################################.....] 95*\n"
+
+sleep 0.3
+clear
+
+printf "\n\033[01;33m Carregando Kerberos...\n"
+printf "[#######################################..] 98*\n"
+
+sleep 0.3
+clear
+
+printf "\n\033[01;33m Carregando Kerberos...\n"
+printf  "[########################################.] 99*\n"
+
+sleep 0.3
+clear
+
+printf "\n\033[01;33m Carregando Kerberos...\n"
+printf "[########################################] 100*\n"
+
+sleep 1
+clear
+
+}
+
+load
---
+++ retorno.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#Retorno para o inicio do Programa
+
+cd ..
+chmod +x KerberosSec.sh
+bash KerberosSec.sh
+
